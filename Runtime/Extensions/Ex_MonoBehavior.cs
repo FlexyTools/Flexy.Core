bcd10a7090df3df57d83986cd93083617c0e9190
JesOb <JesObertyukh@gmail.com>
2025-12-04 23:09:55 +0100

    Added Instantiate Async as MonoBeh Extension

--- Runtime/Extensions/Ex_MonoBehavior.cs
+++ Runtime/Extensions/Ex_MonoBehavior.cs
@@ -2,7 +2,17 @@
 
 public static class Ex_MonoBehavior
 {
-	public static		T? 			OrNull<T>	( this T? obj ) where T: UnityEngine.Object => obj ? obj : null;
-	public static		Boolean		IsAlive		( this UnityEngine.Object? obj ) => obj;
-	public static		void		Destroy		( this UnityEngine.Object obj ) => UnityEngine.Object.Destroy( obj );
+	public static		T? 			OrNull<T>				( this T? obj ) where T: UnityEngine.Object	=> obj ? obj : null;
+	public static		Boolean		IsAlive					( this UnityEngine.Object? obj )			=> obj;
+	public static		void		Destroy					( this UnityEngine.Object obj )				=> UnityEngine.Object.Destroy( obj );
+	public static		T			InstantiateInactive<T>	( this T prefab ) where T : MonoBehaviour	
+	{
+		var active		= prefab.gameObject.activeSelf;
+		prefab.gameObject.SetActive( false );
+		var instance	= UnityEngine.Object.Instantiate( prefab );
+		prefab.gameObject.SetActive( active );
+		prefab.gameObject.ClearEditorDirty();
+		
+		return instance;
+	}
 }
