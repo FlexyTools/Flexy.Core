94d1c5e19d5b7d4117d47fc3cc0694bcf5b10b30
JesOb <JesObertyukh@gmail.com>
2026-01-22 08:23:03 +0100

    Added HierarchyContextDrawer

---
+++ Editor/HierarchyContextDrawer.cs
@@ -0,0 +1,74 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Flexy.Core.Editor;
+
+[InitializeOnLoad]
+public static class HierarchyContextDrawer
+{
+	static HierarchyContextDrawer()
+	{
+		EditorApplication.hierarchyWindowItemOnGUI -= OnHierarchyGUI;
+		EditorApplication.hierarchyWindowItemOnGUI += OnHierarchyGUI;
+	}
+	
+	private static GUIStyle? _rightAlignedStyle;
+	
+	private static void OnHierarchyGUI( Int32 instanceID, Rect selectionRect )
+	{
+		if (!EditorApplication.isPlaying)
+			return;
+	
+		Scene scene = default;
+		var isSceneRow = false;
+
+		var instance = EditorUtility.InstanceIDToObject(instanceID);
+
+		if (instance is not null)
+			return;
+
+		var globalScene = GameContexts.GameContext.Global.gameObject.scene;
+
+		if (globalScene.GetHashCode() == instanceID || globalScene.handle == instanceID)
+		{
+			scene = globalScene;
+			isSceneRow = true;
+		}
+		else
+		{
+			for (var i = 0; i < SceneManager.sceneCount; i++)
+			{
+				var s = SceneManager.GetSceneAt(i);
+				// The instanceID for the Scene header in Hierarchy matches the Scene's handle
+				if (s.GetHashCode() == instanceID || s.handle == instanceID)
+				{
+					scene = s;
+					isSceneRow = true;
+					break;
+				}
+			}
+		}
+
+		if (!isSceneRow)
+			return;
+
+		var ctx = GameContexts.GameContext.GetCtx(scene);
+
+		var labelRect = selectionRect;
+		labelRect.width -= 10;
+		
+		if (_rightAlignedStyle == null)
+		{
+			_rightAlignedStyle = new GUIStyle(EditorStyles.miniLabel)
+			{
+				alignment = TextAnchor.MiddleRight,
+				normal = { textColor = new Color(0.6f, 0.6f, 0.6f) }
+			};
+		}
+		
+		if (GUI.Button(labelRect, ctx.name, _rightAlignedStyle))
+			EditorGUIUtility.PingObject(ctx);
+	}
+}
